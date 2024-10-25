59f2e4aac78ae5caf205d05e8c6d2ca14a4fb18d
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-10-24 22:01:16 -0300

    componente Lista de juegos

---
+++ src/components/Lista.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>Lista de juegos</h3>
+    
+        <table>
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nombre</th>
+                    <th>Stock</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>placeholder</td>
+                    <td>placeholder</td>
+                    <td>placeholder</td>
+                    <td>placeholder</td>
+                    <td>placeholder</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+    table, th, td{
+        border-style: solid;
+        border-color: black;
+        border-width: 1pt;
+        border-radius: 2pt;
+    }
+</style>
